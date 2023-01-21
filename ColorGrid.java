6e28ef279d25c3e7af0c3b46466d23d4c50e04ed
SentientBagel <jhnelson.contact@gmail.com>
2023-01-20 22:03:08 -0500

    adding the files

---
+++ ColorGrid.java
@@ -0,0 +1,172 @@
+package HW1;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JPanel;
+import javax.swing.event.MouseInputListener;
+
+public class ColorGrid extends JPanel implements MouseInputListener, KeyListener {
+
+	GridGameManager game;
+	/**
+	 * Reference to the picker panel, required to update colors
+	 */
+	ColorPickerPanel pickerPanel;
+	
+	int cursorSize = 2;
+	
+	public ColorGrid(int width, int height, Color bgColor)  {
+		setPreferredSize(new Dimension(width, height));
+		setBackground(bgColor); 
+		
+		this.addMouseListener(this);
+		this.addMouseMotionListener(this);
+		
+		this.addKeyListener(this);
+		this.setFocusable(true);
+		this.setFocusTraversalKeysEnabled(false);
+		
+		game = new GridGameManager(width, height);
+	}
+	
+
+	protected void paintComponent(Graphics graphic) {
+		super.paintComponent(graphic);
+		Graphics2D g = (Graphics2D) graphic;
+		
+		game.updateGridDisplay(getWidth(), getHeight());
+		game.drawGrame(g);
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		for(int row = 0; row < game.rows; row++) {
+			for(int col = 0; col < game.cols; col++) {
+				if(game.grid[row][col].contains(e.getPoint())){
+					game.grid[row][col].color = pickerPanel.color;
+					/*
+					 * If you have a 2D array of GridUnits, and you want to paint 1 unit away from the one you've clicked:
+					 * 
+					 * 		1
+					 * 	1	X	1
+					 * 		1
+					 * 
+					 * For 2 units:
+					 * 			2	
+					 * 		2	1	2
+					 * 	2	1	X	1	2
+					 * 		2	1	2
+					 * 			2
+					 * 
+					 * And so forth....
+					 * And of course, you need to check boundaries so you don't go out of bounds on the left/right
+					 */		
+					for(int i = 0; i <= cursorSize; i++) {
+//						if(row + i < game.rows) {
+//							game.grid[row + i][col].color = pickerPanel.color;
+//						}
+//						if(row - i >= 0) {
+//							game.grid[row - i][col].color = pickerPanel.color;
+//						}
+//						if(col + i < game.cols) {
+//							game.grid[row][col + i].color = pickerPanel.color;
+//						}
+//						if(col - i >= 0) {
+//							game.grid[row][col - i].color = pickerPanel.color;
+//						}
+//						if(row + i < game.rows && col + i < game.cols) {
+//							game.grid[row + i][col + i].color = pickerPanel.color;
+//						}
+//						if(row + i < game.rows && col - i >= 0) {
+//							game.grid[row + i][col - i].color = pickerPanel.color;
+//						}
+//						if(row - i >= 0 && col - i >= 0) {
+//							game.grid[row - i][col - i].color = pickerPanel.color;
+//						}
+//						if(row - i >= 0 && col + i < game.cols) {
+//							game.grid[row - i][col + i].color = pickerPanel.color;
+//						}
+						
+						//You could also remove all the if-statements for exception handling:
+						try {
+							game.grid[row + i][col].color = pickerPanel.color;
+							game.grid[row - i][col].color = pickerPanel.color;
+							game.grid[row][col + i].color = pickerPanel.color;
+							game.grid[row][col - i].color = pickerPanel.color;
+							game.grid[row + i][col + i].color = pickerPanel.color;
+							game.grid[row + i][col - i].color = pickerPanel.color;
+							game.grid[row - i][col - i].color = pickerPanel.color;
+							game.grid[row - i][col + i].color = pickerPanel.color;
+						}catch(IndexOutOfBoundsException outOfBounds) {
+							//do nothing...
+						}
+					}
+				}
+			}
+		}
+		repaint();
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent k) {
+		if(k.getKeyCode() == KeyEvent.VK_0) {
+			cursorSize = 0;
+		}else if(k.getKeyCode() == KeyEvent.VK_1) {
+			cursorSize = 1;
+		}else if(k.getKeyCode() == KeyEvent.VK_2) {
+			cursorSize = 2;
+		}else if(k.getKeyCode() == KeyEvent.VK_3) {
+			cursorSize = 3;
+		}else if(k.getKeyCode() == KeyEvent.VK_4) {
+			cursorSize = 4;
+		}
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		
+	}
+}
---
+++ ColorPickerPanel.java
@@ -0,0 +1,183 @@
+package HW1;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class ColorPickerPanel extends JPanel{
+	/*
+	 * It be helpful to name all your UI components with "Label" "Button" etc
+	 * directly in the name, then you can quickly search for them with this.label or whatever
+	 * You can also give them tooltip descriptions, as I've done for the top two below
+	 */
+	/**
+	 * The text of "Current Color: r g b"
+	 */
+	private JLabel currentColorLabel;
+	/**
+	 * The color preview square
+	 */
+	private JLabel currentColorImageLabel;
+	private JLabel redLabel;
+	private JLabel greenLabel;
+	private JLabel blueLabel;
+	private JLabel errorLabel;
+	private JTextField redTextField;
+	private JTextField greenTextField;
+	private JTextField blueTextField;
+	private JButton confirmButton;
+	
+	int red = 100;
+	int green = 215;
+	int blue = 250;
+	
+	public Color color = new Color(red, green, blue);
+	
+	ColorPickerPanel(int width, int height, Color bgColor){
+		setPreferredSize(new Dimension(width, height));
+		setBackground(bgColor); 
+		
+		currentColorLabel = new JLabel("Current Color: R" + red + " G" + green + " B" + blue);
+		currentColorImageLabel = new JLabel();
+		currentColorImageLabel.setOpaque(true);
+		currentColorImageLabel.setBackground(new Color(red, green, blue));
+		redLabel = new JLabel("Red: ");
+		greenLabel = new JLabel("Green: ");
+		blueLabel = new JLabel("Blue: ");
+		errorLabel = new JLabel("");
+		redTextField = new JTextField(""+red);
+		greenTextField = new JTextField(""+green);
+		blueTextField = new JTextField(""+blue);
+		confirmButton = new JButton("Confirm");
+		confirmButton.addActionListener(new ActionListener() { 
+		    public void actionPerformed(ActionEvent e) { 
+		        confirmColor();
+		    } 
+		});
+		
+		this.add(currentColorLabel);
+		this.add(currentColorImageLabel);
+		this.add(redLabel);
+		this.add(greenLabel);
+		this.add(blueLabel);
+		this.add(errorLabel);
+		this.add(redTextField);
+		this.add(greenTextField);
+		this.add(blueTextField);
+		this.add(confirmButton);
+		
+		redTextField.addFocusListener(new FocusListener() {
+		    @Override
+		    public void focusGained(FocusEvent e) { redTextField.setText(""); }
+		    @Override
+		    public void focusLost(FocusEvent e) { }
+		});
+		greenTextField.addFocusListener(new FocusListener() {
+		    @Override
+		    public void focusGained(FocusEvent e) { greenTextField.setText(""); }
+		    @Override
+		    public void focusLost(FocusEvent e) { }
+		});
+		blueTextField.addFocusListener(new FocusListener() {
+		    @Override
+		    public void focusGained(FocusEvent e) { blueTextField.setText(""); }
+		    @Override
+		    public void focusLost(FocusEvent e) { }
+		});
+		
+		confirmButton.setMnemonic(KeyEvent.VK_C);
+		
+		redLabel.setLabelFor(redTextField);
+		greenLabel.setLabelFor(greenTextField);
+		blueLabel.setLabelFor(blueTextField);
+		
+	}
+	
+	public void resizeComponents() {
+		int width = getWidth(); int height = getHeight();
+		int widthUnit = width/20; int heightUnit = height/10;
+		int x = 0; int y = 0;
+		
+		
+		//Row 1
+		y = heightUnit;
+		x = widthUnit;
+		currentColorLabel.setLocation(x, y);
+		currentColorLabel.setSize(widthUnit * 12, heightUnit);
+		x += widthUnit * 12;
+		currentColorImageLabel.setLocation(x, y);
+		currentColorImageLabel.setSize(widthUnit*2, heightUnit);
+	//	currentColorLabel.setFont(new Font("Serif", Font.PLAIN, 15));
+		
+		//Row 2
+		y = heightUnit * 3;
+		x = widthUnit;
+		redLabel.setLocation(x, y);
+		redLabel.setSize(widthUnit * 2, heightUnit);
+		x += widthUnit * 2;
+		redTextField.setLocation(x, y);
+		redTextField.setSize(widthUnit * 2, heightUnit);
+		x += widthUnit * 3;
+		greenLabel.setLocation(x, y);
+		greenLabel.setSize(widthUnit * 2, heightUnit);
+		x += widthUnit * 2;
+		greenTextField.setLocation(x, y);
+		greenTextField.setSize(widthUnit * 2, heightUnit);
+		x += widthUnit * 3;
+		blueLabel.setLocation(x, y);
+		blueLabel.setSize(widthUnit * 2, heightUnit);
+		x += widthUnit * 2;
+		blueTextField.setLocation(x, y);
+		blueTextField.setSize(widthUnit * 2, heightUnit);
+		x += widthUnit * 3;
+		
+		//row 3
+		y = heightUnit * 5;
+		x = widthUnit * 8;
+		confirmButton.setLocation(x, y);
+		confirmButton.setSize(widthUnit * 4, heightUnit);
+		
+		//row 4
+		y = heightUnit * 7;
+		x = widthUnit;
+		errorLabel.setLocation(x, y);
+		errorLabel.setSize(widthUnit * 18, heightUnit);
+	}
+	
+	protected void paintComponent(Graphics graphic) {
+		super.paintComponent(graphic);
+		Graphics2D g = (Graphics2D) graphic;
+		resizeComponents();
+
+	}
+	
+	public void confirmColor() {
+		int oldRed = red; int oldGreen = green; int oldBlue = blue;
+		try {
+			red = Integer.parseInt(this.redTextField.getText().toString());
+			green = Integer.parseInt(this.greenTextField.getText().toString());
+			blue = Integer.parseInt(this.blueTextField.getText().toString());
+			color = new Color(red, green, blue);
+			this.currentColorImageLabel.setBackground(color);
+			errorLabel.setText("Color updated! Previous color was: " + oldRed + 
+					" " + oldGreen + " " + oldBlue);
+			this.currentColorLabel.setText("Current Color: R" + red + " G" + green + " B" + blue);
+		}catch(Exception e) {
+			red = oldRed; green = oldGreen; blue = oldBlue;
+			errorLabel.setText("Not a valid color (0-255 only)");
+			System.out.println("Error entering a color");
+		}
+	}
+
+}
